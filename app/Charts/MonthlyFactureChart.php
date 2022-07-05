335ecffb9e5c328637343e36dd6bb663e3f53469
Christy-Banga <61908366+Christy-Banga@users.noreply.github.com>
2022-07-05 18:22:04 +0100

    arrière

--- app/Charts/MonthlyFactureChart.php
+++ app/Charts/MonthlyFactureChart.php
@@ -26,9 +26,9 @@ class MonthlyFactureChart
                         $q->whereYear('created_at', $year);
                     })
                     ->groupBy(DB::raw("Month(created_at)"))
-                    ->orderBy(DB::raw("Month(created_at)"));
-                    /* ->pluck('count','mois');
- */
+                    ->orderBy(DB::raw("Month(created_at)"))
+                    ->pluck('count','mois');
+
 
         for($i=0; $i<12; $i++){
             if(isset($nombreFactureMois[$i+1]))
